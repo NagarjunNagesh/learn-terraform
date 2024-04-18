476e82e0bceb9781286d12a29c923f3f6133fa14
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2024-04-18 16:26:06 +0200

    sqs queue

---
+++ sqs-queue/outputs.tf
@@ -0,0 +1,7 @@
+output "sqs-queue-arn" {
+    value = aws_sqs_queue.queue.arn
+}
+
+output "sqs-queue2-arn" {
+    value = aws_sqs_queue.queue2.arn
+}
---
+++ sqs-queue/terraform.tfvars
@@ -0,0 +1,3 @@
+region     = "us-east-1"
+access_key = "AKIAT2HWZBDNR2TS7WUS"
+secret_key = "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"
